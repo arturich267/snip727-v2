06027398cacaebc73219512d9fde0a87fd81e14e
arturich267 <arturich267@yandex.ru>
2025-12-02 22:27:48 +0500

    cleanup: remove broken cto.new files

--- src/snip727/web3/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Web3 integration module."""
-from web3 import Web3
-
-from snip727.core.config import get_settings
-
-settings = get_settings()
-
-w3 = Web3(Web3.HTTPProvider(settings.web3_provider_url))
